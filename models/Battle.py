1bb33ab12eab7b49be13ac570317d258c53bdaac
halcolo <juandiego.alfonsoocampo@gmail.com>
2021-04-11 15:37:43 -0500

    Ordering models of pokemons and Battle

--- models/Battle.py
+++ models/Battle.py
@@ -40,30 +40,6 @@ class Battle:
         else:
             print(f"Double KO!!")
 
-
-class Pokemon:
-    def __init__ (self, name:str, level:int,
-                type1, type2):
-        self.name = name
-        self.level = level
-        self.type1 = type1
-        self.type2 = type2
-        self.attacks = []
-        self.stats = {}
-        self.current_status = 0
-        self.current_hp = 0
-
-
-class Attack:
-    def __init__ (self, name, attack_type, category, pp, power, accuracy):
-        self.name = name
-        self.type = attack_type
-        self.category = category
-        self.pp = pp
-        self.power = power
-        self.accuracy = accuracy
-
-
 class Turn:
 
     def __init__(self):
