02a24f01801558e5f00e1d0475279fc6af276713
halcolo <juandiego.alfonsoocampo@gmail.com>
2021-04-12 18:34:06 -0500

    Sending Phase 3

--- models/Battle.py
+++ models/Battle.py
@@ -1,3 +1,5 @@
+import random 
+from models.Pokemon import Pokemon
 from constants import *
 
 class Battle:
@@ -14,20 +16,6 @@ class Battle:
 
         return finised
 
-    def execute_turn(self, turn):
-        command1 = turn.command1
-        command2 = turn.command2
-        attack1 = None
-        attack2 = None
-        if DO_ATTACK  in command1.action.keys():
-            attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
-        if DO_ATTACK  in command2.action.keys():
-            attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
-        self.pokemon2.current_hp -= attack1.power
-        self.pokemon1.current_hp -= attack2.power
-
-        self.actual_turn += 1
-
     def print_current_status(self):
         print(f"{self.pokemon1.name} has {self.pokemon1.current_hp} left")
         print(f"{self.pokemon2.name} has {self.pokemon2.current_hp} left")
@@ -39,6 +27,52 @@ class Battle:
             print(f"{self.pokemon2.name} has won in {self.actual_turn} turns")
         else:
             print(f"Double KO!!")
+
+    def execute_turn(self, turn):
+        command1 = turn.command1
+        command2 = turn.command2
+        attack1 = None
+        attack2 = None
+        if DO_ATTACK  in command1.action.keys():
+            attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
+        if DO_ATTACK  in command2.action.keys():
+            attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
+        self.pokemon2.current_hp -= self.compute_damage(attack1, self.pokemon1, self.pokemon2)
+        self.pokemon1.current_hp -= self.compute_damage(attack2, self.pokemon2, self.pokemon1)
+
+        self.actual_turn += 1
+
+    def compute_damage(self, attack, pokemon1, pokemon2):
+        aux = ((2 * pokemon1.level)/5) + 2
+        powerFactor = aux * attack.power
+        if attack.type == PHYSICAL:
+            print("Physical attack")
+            powerFactor *= (pokemon1.stats[ATTACK]/pokemon2.stats[DEFENSE])
+        else:
+            powerFactor *= (pokemon1.stats[ATTACK]/pokemon2.stats[SPDEFENSE])
+
+        damage_without_modified = powerFactor / 50 +2
+        final_damage = damage_without_modified * self.compute_damage_modifier(attack, pokemon1, pokemon2)
+        print(final_damage)
+        return final_damage
+
+    def compute_damage_modifier(self, attack, pokemon1, pokemon2):
+        #compute STAB
+        stab = 1
+        if attack.type == pokemon1.type1 or attack.type == pokemon1.type2:
+            print("HAS STAB")
+            stab = 1.5
+        # Compute type effectives
+        effectiveness1 = TYPE_CHART[pokemon2.type1][attack.type]
+        effectiveness2 = TYPE_CHART[pokemon2.type2][attack.type]
+        effectiveness_final = effectiveness1 + effectiveness2
+        print(effectiveness1, effectiveness2)
+        critical = 1
+        if random.random() <= 1:
+            print(f"{pokemon1.name} did a critical attack")
+            critical = 1.5
+        return stab * effectiveness_final * critical
+
 
 class Turn:
 
